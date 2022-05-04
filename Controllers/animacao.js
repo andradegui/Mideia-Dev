57beb8b59663b856af0309edd07809a0edee9931
Guilherme <guilima102013@gmail.com>
2022-05-04 09:24:59 -0300

    Telas Login e Cadastro Feitas

--- Controllers/animacao.js
+++ Controllers/animacao.js
@@ -1,11 +1,11 @@
-const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
+const signUpButton = document.getElementById("signUp");
 const container = document.getElementById("container");
 
 signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
+    container.classList.add("right-panel-active");    
 });
 
 signInButton.addEventListener('click', () => {
-    container.classList.remove("left-panel-active");
+    container.classList.remove("right-panel-active");    
 });
